7429a60cc121f53557ef4c697dae2a6ef51c0bcd
giova <giovanaftiburtino@gmail.com>
2022-08-31 23:10:16 -0300

    Exercicio a009 feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,17 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log(minhaString.length)
+
+//a) Remova o excesso de espaços no final da string;
+const removerExcesso = (minhaString.trim())
+
+console.log(removerExcesso)
+
+//b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+console.log(removerExcesso.length)
+
+//c) Substitua os traços `________` por “sticioso”.
+const novaString = minhaString.replaceAll("________", "sticioso")
+
+console.log(novaString)
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,18 @@
-//Crie a const para a frase aqui
+//a) Crie uma `const` no seu código para guardar a frase (com aspas e tudo);
+let guardaFrase = ("Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"")
+
+//b) Crie uma nova string a partir da primeira, trocando **verde** por **rosa**, e **azul** por **laranja**;
+let novaFrase = guardaFrase.replaceAll("verde", "rosa").replace("azul", "laranja")
+
+//c) Verifique se a nova string inclui **verde**, e se inclui **laranja**.
+let checarVerde = novaFrase.includes("verde")
+let checarLaranja = novaFrase.includes("laranja")
+
+//**EXTRA:** tente fazer o “mas não deixe o gato sair” ficar em maiúsculo, assim como o “BOAS VINDAS”
+let fraseMaiuscula = guardaFrase.toUpperCase()
+
+console.log(guardaFrase)
+console.log(novaFrase)
+console.log("A frase inclui verde?", checarVerde)
+console.log("A frase inclui laranja?", checarLaranja)
+console.log(fraseMaiuscula)
